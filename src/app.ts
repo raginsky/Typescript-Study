8641e5f75e44b4d3be957bc06d2f49687e4611ea
Michael <michelr@proftit.com>
2021-12-02 13:47:47 +0200

    classes

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,36 @@
-class Department {
-    // was moved right to constructor
-    // private readonly id: string;
-    // private name: string;
-    private employees: string[] = [];
+abstract class Department {
+    static financialYear = new Date().getFullYear();
+    /**
+     * was moved right to constructor
+     * private readonly id: string;
+     * private name: string;
+     * private employees: string[] = [];
+     * protected instead of private can be inherit in extended class
+     */
+    protected employees: string[] = [];
 
-    constructor(private readonly id: string, public name: string) {
-        // redundant since declared before
-        // this.id = id;
-        // this.name = n;
+    constructor(protected readonly id: string, public name: string) {
+        /**
+         * redundant since declared before
+         * this.id = id;
+         * this.name = n;
+         * can be accessed with class name from own constructor:
+         * console.log(Department.financialYear);
+         */
     }
 
-    describe(this: Department) {
-        console.log(`Department (${this.id}): ${this.name}`);
+    static createEmployee(name: string) {
+        return {name: name};
     }
+
+    /** Now describe() should be presented in all other class that's extends from here */
+    abstract describe(this: Department): void;
 
     addEmployee(employee: string) {
-        // since id is readonly - it can be set only once during initialization
-        // this.id = 'd2';
+        /**
+         * since id is readonly - it can be set only once during initialization
+         * this.id = 'd2';
+         */
         this.employees.push(employee);
     }
 
@@ -26,15 +40,116 @@ class Department {
     }
 }
 
-const accounting = new Department('d1', 'Accounting');
-accounting.addEmployee('David');
-accounting.addEmployee('Zoey');
+class ITDepartment extends Department {
+    admins: string[];
 
-// Will not work since property was marked as private
-// accounting.employees[2] = 'Anna';
+    constructor(id: string, admins: string[]) {
+        super(id, 'IT');
+        this.admins = admins;
+    }
 
-accounting.describe();
-accounting.printEmployeeInfo();
+    describe() {
+        console.log(`IT Department - ID: ${this.id}`);
+    }
+}
 
-// const accountingCopy = { name: 'AccountingCopy', describe: accounting.describe};
-// accountingCopy.describe();
+class AccountingDepartment extends Department {
+    private static instance: AccountingDepartment;
+    private lastReport: string;
+
+    private constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting');
+        this.lastReport = reports[0];
+    }
+
+    /** getter (method that must return something) */
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found.');
+    }
+
+    /** setter (must take a value) */
+    set mostRecentReport(value: string) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value);
+    }
+
+    static getInstance() {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment('d2', []);
+        return this.instance
+    }
+
+    describe() {
+        console.log(`IT Department - ID: ${this.id}`);
+    }
+
+    addEmployee(name: string) {
+        if (name === 'Max') {
+            return;
+        }
+        /** can't inherit if private */
+        this.employees.push(name);
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+        this.lastReport = text;
+    }
+
+    printReports() {
+        console.log(`Printed Reports: `, this.reports);
+        console.log(`Last Report: `, this.lastReport);
+    }
+}
+
+/**
+ * Singleton pattern: Now accessed by static instance since constructor is private:
+ * const accountingDepartment = new AccountingDepartment('d2', []);
+ * Now instance of this class is restrict to single one:
+ */
+const accountingDepartment = AccountingDepartment.getInstance();
+accountingDepartment.addReport('Something went wrong...');
+accountingDepartment.addEmployee('Max');
+accountingDepartment.addEmployee('Dan');
+accountingDepartment.printEmployeeInfo();
+
+/** getter should be accessed as a normal property */
+console.log(accountingDepartment.mostRecentReport);
+/**
+ * setter, must be accessed as a property
+ * this will throw an Error
+ * accountingDepartment.mostRecentReport = '';
+ */
+
+accountingDepartment.mostRecentReport = 'End Year Report';
+accountingDepartment.printReports();
+
+/** static method AND property can be called directly on class without `New` */
+const employee1 = Department.createEmployee('Max');
+console.log(employee1, Department.financialYear);
+
+const IT = new ITDepartment('d1', ['Dylan']);
+IT.describe();
+
+/** Cant be created now because of abstract
+ * const accounting = new Department('d1', 'Accounting');
+ * accounting.addEmployee('David');
+ * accounting.addEmployee('Zoey');
+ */
+
+/**
+ * Will not work since property was marked as private
+ * accounting.employees[2] = 'Anna';
+ */
+
+/**
+ * const accountingCopy = { name: 'AccountingCopy', describe: accounting.describe};
+ * accountingCopy.describe();
+ */
