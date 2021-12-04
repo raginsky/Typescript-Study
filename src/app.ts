edb71ed11a3df0db57a412e1256fffec94ecead0
Michael <michelr@proftit.com>
2021-12-04 17:38:02 +0200

    interfaces part finished

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,51 @@
-/** Let's see interface as custom type */
-interface Greetable {
-    name: string;
+interface AddFn {
+    (a: number, b: number): number;
+}
 
+/** So now we have like anonymous function inside interface */
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+    return n1 + n2;
+}
+
+/** Let's see interface as custom type */
+interface Named {
+    readonly name: string;
+    /** Optional property (methods can be marked too) */
+    readonly optional?: string;
+    outputName?: string;
+}
+
+interface Greetable extends Named {
     greet(phrase: string): void;
 }
 
 class Person implements Greetable {
     name: string;
+    optional?: string;
     age = 30;
 
-    constructor(n: string) {
+    constructor(n: string, o?: string) {
         this.name = n;
+        /** Need to be checked if exist since optional */
+        if (o) {
+            this.optional = o;
+        }
     }
 
     greet(phrase: string) {
-        console.log(phrase + ' ' + this.name);
+        console.log(`${phrase} ${this.name}`);
     }
 }
 
 let user1: Greetable;
 
 user1 = new Person('Joe');
+
+/** Can't be changed outside of the class since property is readonly
+ * user1.name = "Name";
+ */
 
 /** We can be sure that user will have greet() method since it uses Interface with this method */
 user1.greet(`Hi there! I'm`);
