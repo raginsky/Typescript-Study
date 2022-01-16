b1b6e2da8c80d6c8ea772a135998f099846c0078
Michael <michelr@proftit.com>
2022-01-16 17:39:31 +0200

    fetching and binding user input

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,77 @@
+/** Autobind Decorator */
+function Binding(_: any, _2: any, descriptor: PropertyDescriptor) {
+    const initMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            return initMethod.bind(this);
+        }
+    }
+    return adjDescriptor;
+}
+
+/** ProjectInput Class */
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
-        this.templateElement = document.getElementById('project_input' )! as HTMLTemplateElement;
-        this.hostElement = document.getElementById('app' )! as HTMLDivElement;
+        this.templateElement = document.getElementById('project_input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user_input';
+
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDesc = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        if (
+            enteredTitle.trim().length === 0 ||
+            enteredDesc.trim().length === 0 ||
+            enteredPeople.trim().length === 0
+        ) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, enteredDesc, +enteredPeople];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
+    }
+
+    @Binding
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 
     private attach() {
