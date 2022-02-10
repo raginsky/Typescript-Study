0730d359039572b15bb63fc6baef40ed4fa3c168
Michael <michelr@proftit.com>
2022-02-10 17:48:07 +0200

    third party libraries wip

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,30 @@
-import {ProjectInput} from "./components/project-input";
-import {ProjectList} from "./components/project-list";
+import _ from 'lodash';
+// import 'reflect-metadata';
+import {plainToClass} from 'class-transformer';
+import {Product} from "./product.model";
 
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
+console.log(_.shuffle([1, 2, 3]));
+
+declare var GLOBAL: any;
+console.log(GLOBAL);
+
+const products = [
+    {title: 'A Carpet', price: 29.99},
+    {title: 'A Book', price: 10.99}
+];
+
+// const p1 = new Product('A book', 12.99);
+
+// console.log(p1.getInformation());
+
+// const loadedProducts = products.map(prod => {
+//     return new Product(prod.title, prod.price);
+// })
+// for (const prod of loadedProducts) {
+//     console.log(prod.getInformation());
+// }
+
+const loadedProducts = plainToClass(Product, products);
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}
