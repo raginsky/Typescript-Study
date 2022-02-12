51c35b49095226c383a24ef4bcd9508f06e77e4b
Michael <michelr@proftit.com>
2022-02-12 12:20:55 +0200

    react todo-list project finished

---
+++ react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,27 @@
+import React, {useRef} from "react";
+
+import './NewTodo.css';
+
+type NewTodoProps = {
+    onAddTodo: (todoText: string) => void;
+}
+
+const NewTodo: React.FC<NewTodoProps> = props => {
+    const textInputRef = useRef<HTMLInputElement>(null);
+
+    const todoSubmitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        const enteredText = textInputRef.current!.value;
+        props.onAddTodo(enteredText);
+    };
+
+    return <form onSubmit={todoSubmitHandler}>
+        <div className="form-control">
+            <label htmlFor="todo-text">Todo Text</label>
+            <input type="text" id="todo_text" ref={textInputRef}/>
+        </div>
+        <button>ADD TODO</button>
+    </form>;
+};
+
+export default NewTodo
